31bae6d23c3a7cf186d3f129a300891fb269baab
Swarup Shekhar <shekharswarup67@gmail.com>
2026-03-17 15:46:53 +0530

    Updated with google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,18 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               </AuthProvider>
             </QueryProvider>
           </StyledComponentsRegistry>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-N9YTHTRMH1"
+          />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N9YTHTRMH1');
+            `}
+          </Script>
         </body>
       </html>
     );
@@ -112,6 +124,18 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             }}
           />
           <Analytics />
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-N9YTHTRMH1"
+          />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N9YTHTRMH1');
+            `}
+          </Script>
         </body>
       </html>
     </ClerkProvider>
